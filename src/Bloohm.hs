2b6895fa5dca4cdfe4033b5800e4645d186ed3ff
Shae Erisson <shae@scannedinavian.com>
2021-02-06 16:05:32 -0500

    add giveMeNumber to give Integer from Hash

--- src/Bloohm.hs
+++ src/Bloohm.hs
@@ -3,35 +3,14 @@ module Bloohm where
 import Crypto.Hash (hashWith)
 import Crypto.Hash.Algorithms
 import Data.Bits
-import Data.ByteArray (convert)
 import qualified Data.ByteArray.Encoding as B (Base (..), convertToBase)
-import Data.ByteString
+import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
+import Data.List (foldl')
+import Data.Word (Word8)
 
 doBloohm :: String
 doBloohm = "Bloohm"
-
-data StructuralEngineer = SE
-  { earthquakeCertification :: Bool,
-    californiaEmergencyCertification :: Bool,
-    name :: String
-  }
-  deriving (Eq)
-
--- instance Eq StructuralEngineer where
---   (SE ba1 ba2 n1) == (SE bb1 bb2 n2) =
---     ba1 == bb1 && ba2 == bb2 && n1 == n2
-
--- maryanne :: StructuralEngineer
--- maryanne = SE True True "maryanne"
-
--- aaron = SE True True "aaron"
-
--- data HashAlg = forall alg. HashAlgorithm alg => HashAlg alg
-
--- -- put in a string, run the hash function, get result
--- runhash :: HashAlg -> ByteString -> ByteString
--- runhash (HashAlg hashAlg) v = B.convertToBase B.Base16 $ hashWith hashAlg v
 
 runhashMD2 :: ByteString -> ByteString
 runhashMD2 v = B.convertToBase B.Base16 $ hashWith MD2 v
@@ -44,3 +23,16 @@ runhashMD5 v = B.convertToBase B.Base16 $ hashWith MD5 v
 
 runhashTiger :: ByteString -> ByteString
 runhashTiger v = B.convertToBase B.Base16 $ hashWith Tiger v
+
+roll :: [Word8] -> Integer
+roll = foldr unstep 0
+  where
+    unstep b a = a `shiftL` 8 .|. fromIntegral b
+
+roll' :: [Word8] -> Integer
+roll' = foldl' unstep 0
+  where
+    unstep a b = a `shiftL` 8 .|. fromIntegral b
+
+giveMeNumber :: ByteString -> Integer
+giveMeNumber bs = roll $ BS.unpack bs
