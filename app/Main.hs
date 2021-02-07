0e5ccb03bb9c9b4c08b15150bd5c8001d5698e81
Shae Erisson <shae@scannedinavian.com>
2021-02-07 15:31:27 -0500

    circuit python wants \r after input, works now

--- app/Main.hs
+++ app/Main.hs
@@ -6,28 +6,22 @@ module Main where
 
 import Bloohm (findPos)
 import qualified Data.ByteString.Char8 as B
+import Data.List
 import ReadArgs (readArgs)
 import System.Environment (getArgs)
 import System.Hardware.Serialport
-  ( CommSpeed (CS115200),
-    SerialPortSettings (commSpeed),
-    closeSerial,
-    defaultSerialSettings,
-    openSerial,
-    send,
-  )
-import System.IO ()
+import System.IO
 
 main = do
   (serialPort, color :: Color, dir, cmdline) <- readArgs
   let indices = findPos $ dir <> cmdline
-      c = B.pack $ show color
-      combined = (c <>) <$> (B.pack . show <$> indices) -- 1,13 -> r1,r13
-      total = B.intercalate "," combined
-  s <- openSerial serialPort $ defaultSerialSettings {commSpeed = CS115200}
-  print $ "sending " <> total <> " to " <> B.pack serialPort
-  send s total
-  closeSerial s
+      c = show color
+      combined = (c <>) <$> (show <$> indices) -- 1,13 -> r1,r13
+      total = intercalate "," combined
+  h <- hOpenSerial serialPort $ defaultSerialSettings {commSpeed = CS115200}
+  -- print $ "sending " <> total <> " to " <> serialPort
+  hPutStr h (total <> "\r") -- circuit python wants
+  hClose h
 
 data Color = Red | Green | Blue | Yellow deriving (Read, Eq, Ord)
 
