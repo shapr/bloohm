9508dff6a9bb212190427e50b4177df7c0185f59
Shae Erisson <shae@scannedinavian.com>
2021-02-07 16:12:58 -0500

    Update README, include neotrellis code

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # bloohm
 visual bloom filter to display process status as neotrellis m4 output
+
+# Why?
+I like to see process status on my very bright [RGB LED board](https://www.adafruit.com/product/4020).
+Until now, I could only turn the whole board colors, yellow for "process running" with red for "non-zero exit" and green for "zero exit code".
+
+I want the LEDs for output only, I don't want to do any logic on the neotrellis board.
+
+So, I took an idea from [bloom filters](https://en.wikipedia.org/wiki/Bloom_filter) and hash the command line to figure out which LEDs to update with process status colors.
+
+# How to make it go?
+1. Install bloohm, then add the snippet below to your `~/.zshrc` (no idea if this works in bash).
+2. Load the code.py from this repository onto your NeoTrellis M4.
+3. run "try ls", and your status LEDs should briefly turn yellow until the command is done, when they turn green.
+
+# The shell snippet
+
+```zsh
+# for long running commands
+function try () {
+	export thecmd="$*"
+	bloohm "/dev/ttyACM0" Yellow $PWD:A $thecmd
+	time $* && fin || die; # cargo install tally, replacement for time
+}
+
+function play_sound () {
+	aplay -N -q $HOME/.bin/$1.au
+}
+
+function fin () {
+	bloohm "/dev/ttyACM0" Green $PWD:A $thecmd
+	play_sound monkey # monkey.wav from XEmacs https://bitbucket.org/xemacs/sounds-wav/src
+}
+
+function die() {
+	bloohm "/dev/ttyACM0" Red $PWD:A $thecmd
+	play_sound yeep # yeep.wav from XEmacs https://bitbucket.org/xemacs/sounds-wav/src
+	play_sound yeep
+}
+```
---
+++ code.py
@@ -0,0 +1,74 @@
+import time
+import board
+import busio
+import adafruit_trellism4
+import adafruit_adxl34x
+import supervisor
+
+# Set up Trellis and accelerometer
+trellis = adafruit_trellism4.TrellisM4Express(rotation=0)
+i2c = busio.I2C(board.ACCELEROMETER_SCL, board.ACCELEROMETER_SDA)
+sensor = adafruit_adxl34x.ADXL345(i2c)
+
+#define colors
+black = (0,0,0)
+red = (255,0,0)
+green = (0,255,0)
+yellow = (255,255,0)
+off = (16,16,64)
+
+# enable tap to clear
+sensor.enable_tap_detection(threshold=50)
+brightness = 1.0
+trellis.pixels.fill((16,16,64))
+
+# trellis.pixels.brightness(1) # full brightness on the LEDs, values go from 0 - 1, 0.3 is 30% brightness
+pixlsh = [[x,y] for x in range(trellis.pixels.width)  for y in range(trellis.pixels.height) ]
+pixlsw = [[x,y] for y in range(trellis.pixels.height) for x in range(trellis.pixels.width)  ]
+
+def linear_to_pair(n):
+    try:
+        ival = int(n)
+    except ValueError:
+        return (0,0)
+    x = ival % trellis.pixels.width
+    y = ival // trellis.pixels.width
+    return (x,y)
+
+def countup(color,delay=0.01):
+    [setloc(xy[0],xy[1],color,delay) for xy in pixlsh]
+    [setloc(xy[0],xy[1],color,delay) for xy in pixlsw]
+
+def setloc(x,y,color,delay):
+    trellis.pixels[x,y] = color
+    time.sleep(delay)
+
+def color(c):
+    trellis.pixels.fill(c)
+
+def color_from_input():
+    data = input().strip()
+    color = off
+    for l in data.split(","):
+        if l[0] == "r":
+            color = red
+        elif l[0] == "g":
+            color = green
+        elif l[0] == "y":
+            color = yellow
+        trellis.pixels[linear_to_pair(l[1:])] = color
+
+while True:
+    pressed = trellis.pressed_keys
+    if pressed:
+        button_x_value = pressed[0][0] # first element of the list, first element of the x,y pair, should be 0-7
+        brightness = (button_x_value + 3) / 10
+        trellis.pixels.brightness = brightness
+        print("set trellis brightness to",brightness)
+        time.sleep(0.1)
+    if supervisor.runtime.serial_bytes_available:
+        color_from_input()
+    # check tap, no colors when detected
+    if sensor.events['tap']:
+        countup((16,16,64))
+        countup((5,5,5))
