481570549a5fb4ddcc5c24e1d31639ff36fe95bd
Shae Erisson <shae@scannedinavian.com>
2024-03-23 19:04:43 -0400

    switch to flakes

--- default.nix
+++ default.nix
@@ -1,49 +1,10 @@
-{ compiler ? "ghc8103" }:
-
-let
-  sources = import ./nix/sources.nix;
-  pkgs = import sources.nixpkgs {};
-
-  gitignore = pkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
-
-  myHaskellPackages = pkgs.haskell.packages.${compiler}.override {
-    overrides = hself: hsuper: {
-      "bloohm" =
-        hself.callCabal2nix
-          "bloohm"
-          (gitignore ./.)
-          {};
-    };
-  };
-
-  shell = myHaskellPackages.shellFor {
-    packages = p: [
-      p."bloohm"
-    ];
-    buildInputs = [
-      myHaskellPackages.haskell-language-server
-      pkgs.haskellPackages.cabal-install
-      pkgs.haskellPackages.ghcid
-      pkgs.haskellPackages.ormolu
-      pkgs.haskellPackages.hlint
-      pkgs.haskellPackages.hasktags
-      pkgs.niv
-      pkgs.nixpkgs-fmt
-    ];
-    withHoogle = true;
-  };
-
-  exe = pkgs.haskell.lib.justStaticExecutables (myHaskellPackages."bloohm");
-
-  docker = pkgs.dockerTools.buildImage {
-    name = "bloohm";
-    config.Cmd = [ "${exe}/bin/bloohm" ];
-  };
-in
-{
-  inherit shell;
-  inherit exe;
-  inherit docker;
-  inherit myHaskellPackages;
-  "bloohm" = myHaskellPackages."bloohm";
-}
+(import
+  (
+    let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  { src = ./.; }
+).defaultNix
---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  inputs = {
+    # Temporary until `haskell-updates` is merged. Move it back to `nixpkgs-unstable` once it is.
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils/main";
+    flake-compat = {
+      url = "github:edolstra/flake-compat/master";
+      flake = false;
+    };
+  };
+
+  description =
+    "process status display with automatic layout.";
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    let
+      overlay = se: su: {
+        haskellPackages = su.haskellPackages.override {
+          overrides = hse: _hsu: {
+            serialport.meta.broken = false;
+            "bloohm" = hse.callCabal2nix "bloohm" self { };
+          };
+        };
+        bloohm = se.haskell.lib.justStaticExecutables se.haskellPackages.bloohm;
+      };
+    in {
+      inherit overlay;
+    } // flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ overlay ];
+        };
+      in {
+
+        defaultPackage = pkgs.bloohm;
+        devShell = pkgs.haskellPackages.shellFor {
+          packages = p: [ p."bloohm" ];
+          buildInputs = [
+            pkgs.haskellPackages.haskell-language-server
+            pkgs.haskellPackages.cabal-install
+            pkgs.haskellPackages.ghcid
+            pkgs.haskellPackages.ormolu
+            pkgs.haskellPackages.hlint
+            pkgs.nixpkgs-fmt
+            pkgs.bash
+          ];
+          withHoogle = false;
+        };
+      });
+}
